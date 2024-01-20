63bd430736fc9f8e54eb352adc645f3460cef66d
Steve Kamanke <pstevek@gmail.com>
2024-01-20 03:51:29 +0200

    Revamping seeding approach | Minor logging updates

--- src/app/api/middlewares/logging.py
+++ src/app/api/middlewares/logging.py
@@ -23,7 +23,7 @@ async def log_request_middleware(request: Request, call_next):
     logger.info(
         f"{host}:{port} - {timestamp} - "
         f"\"{request.method} {url}\" - "
-        f"{response.status_code} {status_phrase} {formatted_process_time}ms"
+        f"{response.status_code} {status_phrase} {formatted_process_time} ms"
     )
 
     return response
