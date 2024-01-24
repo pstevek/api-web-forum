854c98214905ad4d9599e89cf7e844c0425f1bba
Steve Kamanke <pstevek@gmail.com>
2024-01-24 14:43:01 +0200

    updates

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,7 +6,7 @@ set -o pipefail
 echo "\n> Destroy all Docker dependencies"
 docker compose down --rmi all -v --remove-orphans
 echo "\n> Environment variables clean up"
-rm -f .env
+rm -rf .env
 echo "\n> Remove all caching arterfacts"
 find . | grep -E "(/__pycache__$|\.pytest_cache$|\.ruff_cache$|\.pyc$|\.pyo$)" | xargs rm -rf
 echo "\n> Datastore / volumes clean up"
